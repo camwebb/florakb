03e6bf28ff70f9bd7f3a91c1300b6a27e0a840f7
Kartika Dwintaputri Siregar <dhitatracker@gmail.com>
2014-02-18 02:35:42 +0700

    Change UI Template

--- flora-kalbar.info/app/view/header.php
+++ flora-kalbar.info/app/view/header.php
@@ -1,23 +1,17 @@
 <div class="wrapper">
     <div id="identity">
         <span id="flora">FLORA</span><span id="kalbar"> KALBAR</span>
-        <h2>Records of Plants of Kalimantan Barat</h2>
+    </div>
+    <div id="slogan">
+        <p>Records of Plants of Kalimantan Barat</p>
     </div>
     
-    <div id="log-btn">
-        <a href="register.html" id="btn-reg" class="btn-box">Register</a>
-        <a href="#" id="btn-login" class="btn-box">Login</a>
-    </div>
-    
-    <div id="log-box">
-        <form>
-            <label for="input-username" class="label-login">Username</label>
-            <input id="input-username" class="input-box" type="text" name="username" />
-            <div class="clear"></div>
-            <label for="input-password" class="label-login">Password</label>
-            <input id="input-password" class="input-box" type="password" name="password" />
-            <div class="clear"></div>
-            <input class="btn-box" id="btn-login_submit" type="submit" value="Login" />
+    <div id="search">  
+        <form role="form">
+            <div class="search-box">
+              <input type="text" placeholder="Search" />
+              <button class="btn btn-search" type="button">Search</button>
+            </div>
         </form>
     </div>
     
--- flora-kalbar.info/app/view/menu.php
+++ flora-kalbar.info/app/view/menu.php
@@ -15,25 +15,34 @@ $page = @$DATA['default']['uri']['page'];
 ?>
 
 <div class="wrapper">
-	<ul>
-		<li class="<?php if (!isset($page)) echo 'selected'?>">
-			<a href="<?=$basedomain?>">Home</a>
-		</li>
-		<li class="<?php if ($page=='specimen') echo 'selected'?>">
-			<a href="<?=$basedomain?>specimen">Specimen</a>
-		</li>
-		<li class="<?php if ($page == 'services') echo 'selected'?>">
-			<a href="<?=$basedomain?>services/">Services</a>
-		</li>
-		<li class="<?php if ($page == 'contact') echo 'selected'?>">
-			<a href="<?=$basedomain?>contact">Contact</a>
-		</li>
-		<li class="<?php if ($page == 'news') echo 'selected'?>">
-			<a href="<?=$basedomain?>news">News</a>
-		</li>
-        <li class="<?php if ($page == 'upload') echo 'selected'?>">
-			<a href="<?=$basedomain?>upload">Upload</a>
-		</li>
-        <div class="clear"></div>
-	</ul>
+<div id="nav-box">
+	<div id="menu">
+        <ul>
+    		<li class="<?php if (!isset($page)) echo 'selected'?>">
+    			<a href="<?=$basedomain?>">Home</a>
+    		</li>
+    		<li class="<?php if ($page=='specimen') echo 'selected'?>">
+    			<a href="<?=$basedomain?>specimen">Specimen</a>
+    		</li>
+    		<li class="<?php if ($page == 'services') echo 'selected'?>">
+    			<a href="<?=$basedomain?>services/">Services</a>
+    		</li>
+    		<li class="<?php if ($page == 'contact') echo 'selected'?>">
+    			<a href="<?=$basedomain?>contact">Contact</a>
+    		</li>
+    		<li class="<?php if ($page == 'news') echo 'selected'?>">
+    			<a href="<?=$basedomain?>news">News</a>
+    		</li>
+            <li class="<?php if ($page == 'upload') echo 'selected'?>">
+    			<a href="<?=$basedomain?>upload">Upload</a>
+    		</li>
+    	</ul>
+    </div>
+    
+    <div id="login"> 
+        <a href="#signup-modal" class="btn signup">Sign Up</a> 
+        <a href="#login-modal" class="btn login">Login</a> 
+    </div>
+    <div class="clear"></div>
+</div>
 </div>
