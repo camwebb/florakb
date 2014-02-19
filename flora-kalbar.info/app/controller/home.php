3e262532b5a78ea39ea494b49b15670d327472bf
Ovan Pulu <ovan89@gmail.com>
2014-02-19 11:42:21 +0700

    [excel template engine]

--- flora-kalbar.info/app/controller/home.php
+++ flora-kalbar.info/app/controller/home.php
@@ -4,55 +4,122 @@ defined ('MICRODATA') or exit ( 'Forbidden Access' );
 class home extends Controller {
 	
 	var $models = FALSE;
+	var $view;
 	
-	public function __construct()
+	function __construct()
 	{
+		global $basedomain;
 		$this->loadmodule();
-		// pr($this->view);
-		// exit;
-		// $this->validatePage();
+		$this->view = $this->setSmarty();
+		$this->view->assign('basedomain',$basedomain);
 	}
-	public function loadmodule()
+	
+	function loadmodule()
 	{
 		
 		// $this->models = $this->loadModel('frontend');
 	}
 	
-	public function index(){
+	function index(){
         
 		$var = array(1,2,3);
 		
 		
-		vd($this);
-		// $this->view->assign('test',$var);
+		// vd($this);
+		$this->view->assign('test',$var);
 		
 		$var = 'masuk';
 		return $this->loadView('home');
 
 	}
-        
-	public function tangkap(){
-		if(isset($_POST)){
-			// validasi value yang masuk
-		   $x = form_validation($_POST);
-		   
-		   $data['input'] = $this->models->inputData($x['id'],$x['nama'],$x['alamat']);
-		   
-		   /* tampung kembalian data dari fungsi yang dipanggil */
-			//$data['frontend'] = $this->models->get_data_desc();
-		   if($data['input'])
-		   {
-			   //return $this->loadView('display');
-			   global $CONFIG;
-			   redirect($CONFIG['default']['base_url']."display");
-		   }else {
-			   echo "gagal";
-		   }
-		   
-		   //pr($x);
-		}
+	
+	
+	function fetchExcel($sheet=1,$startRow=1,$startCol=0)
+	{
+		global $EXCEL;
+		
+			$data = array();
+			$newData = array();
+			
+			$numberOfSheet = $sheet;
+			$startRowData = $startRow;
+			$startColData = $startCol;
+			
+			// parameternya adalah name dari input type file
+			$excel = $this->excel('tes');
+			
+			if ($excel){
+			
+				for ($i=0; $i<$numberOfSheet; $i++){
+					
+					$data[$i]['sheet'] = $i;
+					
+					// get field name in current sheet
+					$countColl = $excel->colcount($sheet_index=$i);
+					$countRow = $excel->rowcount($sheet_index=$i);
+					if ($countColl>0){
+						for ($a=$startRowData; $a<=$countColl; $a++){
+							$data[$i]['field_name'][] = $excel->val($startRowData, ($a), $i);
+						}
+					}
+					
+					if ($countRow>0){
+						// looping baris
+						for ($a=$startRowData; $a<=$countRow; $a++){
+							
+							// looping kolom
+							
+							for ($b=$startRowData; $b<=$countColl; $b++){
+								
+								$data[$i]['data'][$a][] = $excel->val($a+1, ($b), $i);
+								
+							}
+							
+						}
+					}
+					
+				}
+			}
+			
+			
+			// clean data, if empty pass
+			if ($data){
+				foreach ($data as $key=>$val){
+					
+					
+					foreach ($val['data'] as $keys=>$values){
+						
+						$newData[$key]['sheet'] = $val['sheet'];
+						$newData[$key]['field_name'] = $val['field_name'];
+						
+						if (!empty($values[0])){
+							
+							$newData[$key]['data'][$keys] = $values;
+						}
+					}
+				
+				}
+			}
+			
+			return $newData;
+			
+		
 	}
 	
+	function parseExcel()
+	{
+		global $EXCEL;
+		if ($_FILES){
+			
+			$numberOfSheet = 5;
+			$startRowData = 1;
+			$startColData = 1;
+			
+			$parseExcel = $this->fetchExcel($numberOfSheet,$startRowData,$startColData);
+			
+			pr($parseExcel);
+		}
+	}
 }
 
 ?>
---
+++ flora-kalbar.info/config/coll_conf.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Collection specification */
+
+/* use typedata, length */
+$C_SPEC['unique_key'] 	= 'int, 10';
+$C_SPEC['date'] 		= 'string, 10';
+$C_SPEC['obs'] 			= 'string, 10';
+$C_SPEC['locn'] 		= 'string, 10';
+
+?>
