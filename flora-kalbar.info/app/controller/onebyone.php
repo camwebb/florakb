8439b2879829019b0c4b3c75d33711b73d342088
Fitri Nurjannah <widiw374@gmail.com>
2014-04-23 17:36:54 +0700

    check name exist ajax

--- flora-kalbar.info/app/controller/onebyone.php
+++ flora-kalbar.info/app/controller/onebyone.php
@@ -21,30 +21,43 @@ class onebyone extends Controller {
 	public function loadmodule()
 	{
         $this->insertonebyone = $this->loadModel('insertonebyone');
+        $this->loginHelper = $this->loadModel('loginHelper');
 	}
 	
 	public function index(){
 		return $this->loadView('onebyone');
 	}
     
+    /**
+     * @todo show view for location form
+     * */
     public function location(){
         $msg = $this->msg->display('all', false);
         $this->view->assign('msg', $msg);
         return $this->loadView('formLocation');
     }
     
+    /**
+     * @todo show view for person form
+     * */
     public function person(){
         $msg = $this->msg->display('all', false);
         $this->view->assign('msg', $msg);
         return $this->loadView('formPerson');
     }
     
+    /**
+     * @todo show view for taxon form
+     * */
     public function taxon(){
         $msg = $this->msg->display('all', false);
         $this->view->assign('msg', $msg);
         return $this->loadView('formTaxon');
     }
     
+    /**
+     * @todo insert person from posted data
+     * */
     public function insertPerson(){
         $data = $_POST;
         $insertData = $this->insertonebyone->insertTransaction('person',$data);
@@ -55,6 +68,36 @@ class onebyone extends Controller {
             $this->msg->add('e', 'This is a sample Error Message');
         }
         header('Location: ../onebyone/person');
+    }
+    
+    public function check_Name(){
+        $data = $_POST['name'];
+        $check = $this->loginHelper->checkName($data);
+        if($check){
+            $status = 'success';
+            $msg = '';
+        }else{
+            $status = 'error';
+            $msg = 'Name already exist';
+        }
+        
+        echo json_encode(array('status' => $status, 'message' => $msg));
+        exit;
+    }
+    
+    public function check_Twitter(){
+        $data = $_POST['twitter'];
+        $check = $this->loginHelper->checkTwitter($data);
+        if($check){
+            $status = 'success';
+            $msg = '';
+        }else{
+            $status = 'error';
+            $msg = 'Twitter account exist!';
+        }
+        
+        echo json_encode(array('status' => $status, 'message' => $msg));
+        exit;
     }
 }
 
--- flora-kalbar.info/assets/js/globalHelper.js
+++ flora-kalbar.info/assets/js/globalHelper.js
@@ -1,3 +1,9 @@
+var first_error = '<div class="messages erroren"><a href="#" class="closeMessage"></a><p>';
+var first_info = '<div class="messages info"><a href="#" class="closeMessage"></a><p>';
+var first_success = '<div class="messages success"><a href="#" class="closeMessage"></a><p>';
+var first_warning = '<div class="messages warning"><a href="#" class="closeMessage"></a><p>';
+var end = '</p></div>';
+
 /**
 * checking extention from input file
 * @param inputID = id of input type file
@@ -10,4 +16,19 @@ function hasExtension(inputID, exts) {
 
 function checkName(){
     var name = $('#name').val();
+    var data = { 'name' : name };
+    var check = 
+        $.ajax({
+            type: "POST",
+            url: baseUrl+"onebyone/check_Name",
+            data: data,
+            async: false,
+            success: function(response){}
+        }).responseText;
+        
+    var result = JSON.parse(check);
+    if(result.status == 'error'){
+        // TAMBAH CLASS ERROR DI INPUT BOX
+        $('#nameGroup').append('<label for="name" class="error"> '+ result.message +'</label>');
+    }
 }
