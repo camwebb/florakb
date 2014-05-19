65337360d4715d818aebe40cd17751fd25b73c4c
ovancop <ovan89@gmail.com>
2014-05-19 10:48:07 +0700

    add filetype xls

--- flora-kalbar.info/config/libs.conf.php
+++ flora-kalbar.info/config/libs.conf.php
@@ -5,7 +5,7 @@
 $EXCEL[0]['startrow'] = 2;
 $EXCEL[0]['startcolumn'] = 1;
 $EXCEL[0]['startindex'] = 0;
-$EXCEL[0]['filetype'] = array('application/ms-excel', 'application/vnd.ms-excel');
+$EXCEL[0]['filetype'] = array('application/ms-excel', 'application/vnd.ms-excel','application/octet-stream');
 
 /* SMARTY CONFIG */
 
