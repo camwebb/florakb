bfe7140f217d9606af48280c0716f3bb0cae3876
Ovan Pulu <ovan89@gmail.com>
2014-06-21 09:55:03 +0700

    add uniq index

--- doc/florakb_mail_log.sql
+++ doc/florakb_mail_log.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 19, 2014 at 11:11 PM
+-- Generation Time: Jun 21, 2014 at 10:54 AM
 -- Server version: 5.5.37-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4
 
@@ -32,8 +32,9 @@ CREATE TABLE IF NOT EXISTS `florakb_mail_log` (
   `subject` varchar(100) DEFAULT NULL,
   `send_date` datetime DEFAULT NULL,
   `n_status` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `receipt` (`receipt`,`subject`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
