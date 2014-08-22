785fc0cb649d9a0bfd33b04fb85b4bbc6a4e4f1f
Ovan Pulu <ovan89@gmail.com>
2014-08-22 17:58:00 +0700

    add new field

--- doc/florakb_mail_log.sql
+++ doc/florakb_mail_log.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 21, 2014 at 10:54 AM
+-- Generation Time: Aug 22, 2014 at 06:56 PM
 -- Server version: 5.5.37-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4
+-- PHP Version: 5.5.9-1ubuntu4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -30,11 +30,12 @@ CREATE TABLE IF NOT EXISTS `florakb_mail_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `receipt` varchar(50) DEFAULT NULL,
   `subject` varchar(100) DEFAULT NULL,
+  `encode` text,
   `send_date` datetime DEFAULT NULL,
   `n_status` int(11) DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `receipt` (`receipt`,`subject`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=46 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
